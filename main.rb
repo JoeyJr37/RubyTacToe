47ffed0234d9d44e1ebfc84f7ad4aaec8e59d011
JoeyJr37 <JoeyJr11@gmail.com>
2020-07-05 08:15:56 -0500

    updated logic for tie games

--- main.rb
+++ main.rb
@@ -31,44 +31,50 @@ module Game
         end 
         
         def board_full?
+            counter = 0
+            numbers = (0..9)
             @board_array.each do |element|
-                if element == (0..9)
-                    false
+                if numbers.include?(element) == true
+                    next
                 else
-                    true
+                    counter += 1
                 end
             end
+            "Board full" if counter == 9
         end
 
-    def get_player_one
-        puts "What is Player one's name?"
-        player_one_name = gets.chomp
-        player_one = Player.new(player_one_name, 'X')
-        puts "Greetings #{player_one_name.capitalize}! You are #{player_one.marker}'s."
-        player_one
-    end
+        def get_player_one
+            puts "What is Player one's name?"
+            player_one_name = gets.chomp
+            player_one = Player.new(player_one_name, 'x')
+            puts "Greetings #{player_one_name.capitalize}! You are #{player_one.marker}'s."
+            player_one
+        end
 
-    def get_player_two
-        puts "What is Player two's name?"
-        player_two_name = gets.chomp
-        player_two = Player.new(player_two_name, 'O')
-        puts "Greetings #{player_two_name.capitalize}! You are #{player_two.marker}'s."
-        player_two
-    end
+        def get_player_two
+            puts "What is Player two's name?"
+            player_two_name = gets.chomp
+            player_two = Player.new(player_two_name, 'o')
+            puts "Greetings #{player_two_name.capitalize}! You are #{player_two.marker}'s."
+            player_two
+        end
 
-    def win_condition(player_one, player_two)
-        winning_combinations = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
-        winning_combinations.each do |set| 
+        def win_condition(player_one, player_two)
+            winning_combinations = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[3,5,7],[1,4,7],[2,5,8],[3,6,9]]
+            winning_combinations.each do |set| 
                 if player_one.player_array.sort == set
                     break player_one
                 elsif player_two.player_array.sort == set
                     break player_two
+                elsif board_full? == "Board full"
+                    break "tie"
                 else
                     nil
                 end
             end
+        end
     end
-end
+
     def self.determine_current_player(first_player, second_player)
         if first_player.current_player == true
             first_player.current_player = false
@@ -90,14 +96,18 @@ end
         puts "#{current_player.name.capitalize} please select a number: "
         new_game.display_board()
         new_game.update_game_board(current_player)
-        while new_game.win_condition(player_one, player_two) != player_one && new_game.win_condition(player_one, player_two) != player_two
+        while new_game.win_condition(player_one, player_two) != player_one && new_game.win_condition(player_one, player_two) != player_two && new_game.win_condition(player_one, player_two) != "tie"
             current_player = determine_current_player(player_one, player_two)
             puts "#{current_player.name.capitalize} please select a number: "
             new_game.update_game_board(current_player)
             new_game.win_condition(player_one, player_two)
         end
-        winner = new_game.win_condition(player_one, player_two)
-        puts "#{winner.name.capitalize} is the winner!"
+        if new_game.win_condition(player_one, player_two) == "tie"
+            puts "It's a tie!"
+        else
+            winner = new_game.win_condition(player_one, player_two)
+            puts "#{winner.name.capitalize} is the winner!"
+        end
     end
 end
 
